cbbe218fef9159026df1af80c0ca921e9474946f
N Srimal <43135950+kmnsrimal@users.noreply.github.com>
2020-01-26 23:40:49 +0530

    Create Memory Management.c

---
+++ Memory Management.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define maximum size 
+#define MAX 2600
+
+// Create the memory node
+struct memory {
+    char *proID;
+    int start;
+    int size;
+    int isFree;
+    struct memory *next;
+};
+
+// Starting node 
+struct memory *START = NULL;
